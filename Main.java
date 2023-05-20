2bb7f584bd666fa69243decd81372d11ac1a54dd
Mbkh <mbkh1382@gmail.com>
2023-05-20 23:59:52 +0330

    database updated

--- Main.java
+++ Main.java
@@ -2,8 +2,9 @@ public class Main {
     public static void main(String[] args) {
          Sql s = new Sql();
          s.Select_test();
-         s.Insert_test("mohammad", "bagher");
-         //s.delete_test(2);
-         s.Select_test();
+         //s.Insert_test("akbar", "akbari");
+         //s.Select_test();
+         ////s.delete_test(2);
+         //s.Select_test();
     }
 }
--- Sql.java
+++ Sql.java
@@ -19,17 +19,27 @@ public class Sql {
             ResultSet rs = stm.executeQuery( "SELECT * FROM User;" );
             while ( rs.next() ) {
                int id = rs.getInt("id");
+               String  username = rs.getString("username");
+               String  password = rs.getString("password");
                String  name = rs.getString("name");
-               String fullname  = rs.getString("fullname");
+               String  securityQuestion = rs.getString("securityQuestion");
+               String  securityAnswer = rs.getString("securityAnswer");
+               int  type = rs.getInt("type");
+
                System.out.println( "ID = " + id );
+               System.out.println( "USERNAME = " + username );
+               System.out.println( "PASSWORD = " + password );
                System.out.println( "NAME = " + name );
-               System.out.println( "FULLNAME = " + fullname );
+               System.out.println( "securityQuestion = " + securityQuestion );
+               System.out.println( "securityAnswer = " + securityAnswer );
+               System.out.println( "type = " + type );
+               
                System.out.println();
             }
             rs.close();
             stm.close();
         } catch (SQLException e) {
-            System.out.println("Could not select data from database : Select_test");
+            System.out.println("Could not select data from database : Select_test : "+e.getMessage());
         }
     }
     void Insert_test(String name , String fullname)
