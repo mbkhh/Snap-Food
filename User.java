fc2a6d5a275bde6a6e8cdb31f30d26dc30a942bd
Parhamast <130389105+Parhamast@users.noreply.github.com>
2023-05-24 16:45:56 +0330

    User class created

--- User.java
+++ User.java
@@ -2,6 +2,14 @@
  * User
  */
 public class User {
-    static User currentUser = null;
-    
+    public int id;
+    static User currentUser = null; 
+    public String username ;
+    public String password;
+    public String name;
+    public String securityQuestion;
+    public String securityAnswer;
+    public int type;
+    public int balance;
+    public static ArrayList<User> users = new ArrayList<User>();
 }
